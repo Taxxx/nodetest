7b8e13e8a493bd35b1cccb5a058385e16950b840
Henrry Chuquimia <hrchuquimia@gmail.com>
2017-12-14 05:51:30 -0400

    Added getRatingAvg method to get and update the restaurant rating - Updated readme

--- README.md
+++ README.md
@@ -5,5 +5,6 @@
 * npm install
 * node .
 * Open in a browser to test if the service is running -> <http://localhost:3000/>
-* Open in a browser to see the endpoints documentation, you can test here the service too -> <http://localhost:3000/explorer>
+* Open in a browser to see the endpoints documentation endpoints for restaurant, reviews, meals and orders are generated, you can test here the service too -> <http://localhost:3000/explorer>
 * Open in a browser to test get restaurant with reviews and meals [test](http://localhost:3000/api/restaurants/1?filter[include]=reviews&filter[include]=meals)
+* getRatingAvg  <http://localhost:3000/api/reviews/getRatingAvg?restaurantId=1>
--- common/models/review.js
+++ common/models/review.js
@@ -1,5 +1,45 @@
 'use strict';
+// var Restaurant = require('./restaurant.js');
 
 module.exports = function(Review) {
+  Review.getRatingAvg = function(restaurantId, cb) {
+    Review.find({where: {restaurantId: restaurantId}}, function(err, reviews) {
+      var sum = 0;
+      var avg;
+      var Restaurant = Review.app.models.Restaurant;
+      if (restaurantId) {
+        reviews.forEach(function(restaurant) {
+          sum = sum + restaurant.rating;
+        });
+        avg = sum / reviews.length;
+        Restaurant.findById(restaurantId, function(err, restaurant) {
+          restaurant.updateAttributes({rating: avg});
+          restaurant.save();
+        });
+        cb(null, avg);
+      } else {
+        cb(null, null);
+      }
+    });
+  };
 
+  Review.remoteMethod(
+    'getRatingAvg', {
+      http: {
+        path: '/getRatingAvg',
+        verb: 'get'
+      },
+      accepts: {
+        arg: 'restaurantId',
+        type: 'number',
+        http: {
+          source: 'query'
+        }
+      },
+      returns: {
+        arg: 'ratingAvg',
+        type: 'Array'
+      }
+    }
+  );
 };
